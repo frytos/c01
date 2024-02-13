dd638148958eee7e3c50402e0024c518342eee98
Arnaud Groussard <argrouss@made-f0Br5s3.clusters.42paris.fr>
2024-02-13 13:52:50 +0100

    adds the ft_ultimate_ft.c ex

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: argrouss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 09:10:16 by argrouss          #+#    #+#             */
+/*   Updated: 2024/02/13 10:24:59 by argrouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr);
+	
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+int	main(void)
+{
+	int	*ptr1;
+	int	**ptr2;
+	int	***ptr3;
+	int	****ptr4;
+	int	*****ptr5;
+	int	******ptr6;
+	int	*******ptr7;
+	int	********ptr8;
+	int	*********ptr9;
+	int	value = 9;
+	ptr1 = &value;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	printf("before : Value = %d\n", value);
+	ft_ultimate_ft(ptr9);
+	printf("after : Value = %d\n", value);
+}
