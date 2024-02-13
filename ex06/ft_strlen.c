b15e66265d70ce99199a3b35082eeaccf13034d4
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-14 00:27:55 +0100

    adds ft_strlen.c

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 00:21:27 by argrouss          #+#    #+#             */
+/*   Updated: 2024/02/14 00:26:40 by argrouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_strlen(char *str);
+
+int ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != 0)
+	{
+		len++;
+	}
+	return (len);
+}	
+int	main(void)
+{
+	char	myString[] = "Fryt";
+	printf("Length of %s = %d", myString, ft_strlen(myString));
+	return (0);
+}
